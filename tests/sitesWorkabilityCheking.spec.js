1f12db954a6674d3707417bbc118f8767710d506
ab7nt <ab7nt@mail.ru>
2025-04-09 11:13:05 +0300

    Исправление опечатки в названии тестов

--- tests/sitesWorkabilityCheking.spec.js
+++ tests/sitesWorkabilityCheking.spec.js
@@ -6,42 +6,42 @@ import { OneTmMainPage } from '../PageObjects/OneTmMainPage';
 import { LiteraMainPage } from '../PageObjects/LiteraMainPage';
 import { SequoiaMainPage } from '../PageObjects/SequoiaMainPage';
 
-describe('Проверка рабоспособности сайтов', () => {
-    test('Проверка рабоспособности сайта mdmprint.ru', async ({ page }) => {
+describe('Проверка работоспособности сайтов', () => {
+    test('Проверка работоспособности сайта mdmprint.ru', async ({ page }) => {
         const mdmprintMainPage = new MdmprintMainPage(page);
 
         // Общая проверка работоспособности
         await mdmprintMainPage.generalWorkabilityChecking();
     });
 
-    test('Проверка рабоспособности сайта copy.ru', async ({ page }) => {
+    test('Проверка работоспособности сайта copy.ru', async ({ page }) => {
         const copyRuMainPage = new CopyRuMainPage(page);
 
         // Общая проверка работоспособности
         await copyRuMainPage.generalWorkabilityChecking();
     });
 
-    test('Проверка рабоспособности сайта 1tm.ru', async ({ page }) => {
+    test('Проверка работоспособности сайта 1tm.ru', async ({ page }) => {
         const oneTmMainPage = new OneTmMainPage(page);
 
         // Общая проверка работоспособности
         await oneTmMainPage.generalWorkabilityChecking();
     });
 
-    test('Проверка рабоспособности сайта litera.studio', async ({ page }) => {
+    test('Проверка работоспособности сайта litera.studio', async ({ page }) => {
         const literaMainPage = new LiteraMainPage(page);
 
         // Общая проверка работоспособности
         await literaMainPage.generalWorkabilityChecking();
     });
 
-    test('Проверка рабоспособности сайта vea.ru', async ({ page }) => {
+    test('Проверка работоспособности сайта vea.ru', async ({ page }) => {
         const veaMainPage = new VeaMainPage(page);
         // Общая проверка работоспособности
         await veaMainPage.generalWorkabilityChecking();
     });
 
-    test('Проверка рабоспособности сайта sequoiapay.io', async ({ page }) => {
+    test('Проверка работоспособности сайта sequoiapay.io', async ({ page }) => {
         const sequoiaMainPage = new SequoiaMainPage(page);
 
         // Общая проверка работоспособности
