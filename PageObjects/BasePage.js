600c67a7c3e624383952d61509ab43d163135413
ab7nt <ab7nt@mail.ru>
2025-04-15 15:04:49 +0300

    Корректировка времени, указанного в имени скриншота

--- PageObjects/BasePage.js
+++ PageObjects/BasePage.js
@@ -119,7 +119,15 @@ export class BasePage {
             finalScreenshots.push({
                 name: screenshotName,
                 content: screenshot,
-                timestamp: new Date(Date.now() + 3 * 3600 * 1000).toLocaleString(), // Время по Москве
+                timestamp: new Date(Date.now() + 3 * 3600 * 1000).toLocaleString('ru-RU', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: false,
+                }),
             });
         });
     }
