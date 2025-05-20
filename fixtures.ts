d26609ff128fdeca7d988b9426272d65161be2e8
ab7nt <ab7nt@mail.ru>
2025-05-20 17:11:25 +0300

    Добавление await.init();

--- fixtures.ts
+++ fixtures.ts
@@ -22,31 +22,37 @@ export const test = base.extend<Fixtures>({
     veaMainPage: async ({ page }, use) => {
         const vea = new VeaMainPage(page);
         await vea.open();
+        await vea.init();
         await use(vea);
     },
     mdmprintMainPage: async ({ page }, use) => {
         const mdm = new MdmprintMainPage(page);
         await mdm.open();
+        await mdm.init();
         await use(mdm);
     },
     copyRuMainPage: async ({ page }, use) => {
         const copy = new CopyRuMainPage(page);
         await copy.open();
+        await copy.init();
         await use(copy);
     },
     oneTmMainPage: async ({ page }, use) => {
         const oneTm = new OneTmMainPage(page);
         await oneTm.open();
+        await oneTm.init();
         await use(oneTm);
     },
     literaMainPage: async ({ page }, use) => {
         const litera = new LiteraMainPage(page);
         await litera.open();
+        await litera.init();
         await use(litera);
     },
     sequoiaMainPage: async ({ page }, use) => {
         const sequoia = new SequoiaMainPage(page);
         await sequoia.open();
+        await sequoia.init();
         await use(sequoia);
     },
 
